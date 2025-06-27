7dd23245e3980834847a3cdc923d518fcfdfb4e8
gustavo <tecnicogustavo1b@gmail.com>
2025-06-27 07:35:52 -0300

    Ajustes

--- src/componentes/nossa_equipe.js
+++ src/componentes/nossa_equipe.js
@@ -92,19 +92,4 @@ function loadShow() {
     }
 }
 
-function initNavigation() {
-    document.getElementById("next").addEventListener("click", () => {
-        const items = document.querySelectorAll(".slider .item");
-        if (active < items.length - 1) {
-            active++;
-            loadShow();
-        }
-    });
 
-    document.getElementById("prev").addEventListener("click", () => {
-        if (active > 0) {
-            active--;
-            loadShow();
-        }
-    });
-}
